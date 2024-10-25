ecf851e8b4f2518c16db243497bad5fd91e8d274
wellpaper23 <wevelle98@gmail.com>
2024-10-25 20:39:18 -0300

    Organizando as pastas de teste de personagem

---
+++ src/testesPersonagens/geradorPersonagem.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //gerador de Protagonista
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Qual o seu nome? ");
+        String nomeProtagonista = scanner.nextLine();
+
+        System.out.println("Qual o seu gênero? ");
+        String generoProtagonista = scanner.nextLine();
+
+        System.out.println("Qual a sua idade? ");
+        int idadeProtagonista = scanner.nextInt();
+
+        System.out.println("Qual o seu nivel poder?");
+        int poder = scanner.nextInt();
+
+        System.out.println("Qual a cor do seu cabelo? ");
+        String corCabelo = scanner.nextLine();
+
+        System.out.println("=================================");
+        System.out.println("Você se chama " + nomeProtagonista + " é um(a) " + generoProtagonista + " de cabelos "+ corCabelo + ", possui " + idadeProtagonista + " e nivel de poder igual a " + poder + ".");
+
+    }
+}
