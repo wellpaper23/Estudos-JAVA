8d1ae9d619d9cc8f9bb5bd13d42e9a79de5f14bb
Well <wevelle98@gmail.com>
2025-02-07 00:18:06 -0300

    Lista de clãs funcionando

---
+++ src/DesafioHeranca/Uzumaki.java
@@ -0,0 +1,22 @@
+package DesafioHeranca;
+
+public class Uchiha extends Ninja {
+    String ClaDoNinja = "Uchiha";
+    String habilidadeEspecial;
+    public void mostrarHabilidadeEspecial() {
+        System.out.println("Habilidade Especial: " + habilidadeEspecial);
+
+    }
+
+    public void mostrarInformacoes() {
+
+        System.out.println("nome = " + nome[numeroDoNinja]);
+        System.out.println("Clã: " + ClaDoNinja);
+        System.out.println("idade = " + idade[numeroDoNinja]);
+        System.out.println("Missao = " + missao[numeroDoNinja]);
+        System.out.println("Nivel de dificuldade = " + nivelDeDificuldade[numeroDoNinja]);
+        System.out.println("Status da Missao = " + statusDaMissao[numeroDoNinja]);
+        mostrarHabilidadeEspecial();
+    }
+
+}
